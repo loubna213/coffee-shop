b897b20129d262bae654b7c0d33e96d4d9060ee0
loubna213 <98254998+loubna213@users.noreply.github.com>
2024-12-19 09:39:11 +0100

    Update CoffeeList.tsx

--- src/components/CoffeeList.tsx
+++ src/components/CoffeeList.tsx
@@ -22,7 +22,7 @@ const CoffeeList = () => {
 
 
     return (
-        <section className='max-container relative mt-[-9rem] max-md:mt-[-3rem] rounded-lg bg-dark-200 h-full w-4/5 max-md:w-[90%] flex flex-col justify-center items-center px-6 py-20'>
+        <section className='max-container relative mt-[-9rem] max-md:mt-[-3rem] rounded-lg bg-dark-200 h-full w-4/5 max-md:w-[90%] flex flex-col justify-center items-center px-6 py-24'>
             <img src="/vector.svg" alt="vector" className="absolute top-4 left-[50%] translate-[-50%] z-[2]"/>
             <h1 className="capitalize font-semibold text-white text-4xl mb-4 z-10">Our Collection</h1>
             <p className="text-dark-300 max-w-[520px] text-center z-10">Introducing our Coffee Collection, a selection of unique coffees from different roast types and origins, expertly roasted in small batches and shipped fresh weekly.</p>
@@ -30,7 +30,7 @@ const CoffeeList = () => {
                 <button onClick={() => setContent('all')} className={`${content === 'all' && 'bg-dark-300 p-2 rounded-lg'}`}>All products</button>
                 <button onClick={() => setContent('available')} className={`${content === 'available' && 'bg-dark-300 p-2 rounded-lg'}`}>Available now</button>
             </div>
-            <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10 mt-12">
+            <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-10 mt-8">
                 { content === 'all' ?
                     coffeeList.map((item: Coffee) => <CoffeeItem key={item.id} item={item} />)
                     : coffeeList.map((item: Coffee) => item.available && <CoffeeItem key={item.id} item={item} />)
