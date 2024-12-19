c038ee5854c04d8ea9f2a882052619307ae6fde2
loubna213 <98254998+loubna213@users.noreply.github.com>
2024-12-19 09:45:25 +0100

    Update CoffeeList.tsx

--- src/components/CoffeeList.tsx
+++ src/components/CoffeeList.tsx
@@ -22,7 +22,7 @@ const CoffeeList = () => {
 
 
     return (
-        <section className='max-container relative mt-[-9rem] max-md:mt-[-3rem] rounded-lg bg-dark-200 h-full w-4/5 max-md:w-[90%] flex flex-col justify-center items-center px-6 py-16'>
+        <section className='max-container relative mt-[-9rem] max-md:mt-[-3rem] rounded-lg bg-dark-200 h-full w-4/5 max-md:w-[90%] flex flex-col justify-center items-center px-6 py-[18px]'>
             <img src="/vector.svg" alt="vector" className="absolute top-4 left-[50%] translate-[-50%] z-[2]"/>
             <h1 className="capitalize font-semibold text-white text-4xl mb-4 z-10">Our Collection</h1>
             <p className="text-dark-300 max-w-[520px] text-center z-10">Introducing our Coffee Collection, a selection of unique coffees from different roast types and origins, expertly roasted in small batches and shipped fresh weekly.</p>
