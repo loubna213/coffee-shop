df13af4e91a48bd70c3f9c06e3279c92d87495fc
loubna213 <98254998+loubna213@users.noreply.github.com>
2024-12-20 18:22:34 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import CoffeeList from './components/CoffeeList'
 function App() {
   return (
     <main className="bg-dark-100 w-full min-h-screen pb-16">
-      <img src="/bg-cafe.jpg" alt="coffee" className="" />
+      <img src="/bg-cafe.jpg" alt="coffee"/>
       <CoffeeList/>
     </main>
   )
